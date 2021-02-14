6f607033319c3f86815cde0812f81ed206f7e79a
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-02-14 18:26:04 +0400

    add splash screen.

--- app/src/main/java/com/almissbbah/nytimes/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/almissbbah/nytimes/ui/splash/SplashFragment.kt
@@ -1,6 +1,9 @@
 package com.almissbbah.nytimes.ui.splash
 
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.almissbbah.nytimes.R
 import com.almissbbah.nytimes.ui.AppBaseFragment
 import javax.inject.Inject
@@ -14,20 +17,29 @@ class SplashFragment : AppBaseFragment() {
     private lateinit var splashViewModel: SplashViewModel
 
     override fun initViewModel() {
-
+        splashViewModel =
+            ViewModelProviders.of(
+                this,
+                viewModelFactory
+            )[SplashViewModel::class.java]
     }
 
     override fun initViews() {
     }
 
     override fun subscribe() {
+        splashViewModel.action.observe(viewLifecycleOwner, Observer {
+            findNavController().navigate(R.id.action_nav_splash_to_nav_home)
+        })
+        splashViewModel.startTimer()
     }
 
     override fun unSubscribe() {
+        splashViewModel.unSubscribe()
     }
 
     override fun onCreateView(): Int {
-        return R.layout.fragment_slideshow
+        return R.layout.fragment_splash
     }
 
 
--- app/src/main/java/com/almissbbah/nytimes/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/almissbbah/nytimes/ui/splash/SplashViewModel.kt
@@ -3,12 +3,33 @@ package com.almissbbah.nytimes.ui.splash
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.almissbbah.nytimes.data.Resource
+import com.almissbbah.nytimes.utils.SPLASH_SCREEN_DELAY_MS
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class SplashViewModel @Inject constructor() : ViewModel() {
+    enum class Action { NAVIGATE_TO_HOME_SCREEN }
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+    private var disposable: Disposable? = null
+    private val _action = MutableLiveData<Resource<Any, Action>>()
+    val action: LiveData<Resource<Any, Action>> = _action
+
+    fun startTimer() {
+        showHomeScreenAfterDelay()
     }
-    val text: LiveData<String> = _text
+
+    private fun showHomeScreenAfterDelay() {
+        disposable = Observable.timer(SPLASH_SCREEN_DELAY_MS, TimeUnit.MILLISECONDS)
+            .map {
+                _action.postValue(Resource(null, Action.NAVIGATE_TO_HOME_SCREEN, ""))
+            }.observeOn(AndroidSchedulers.mainThread()).subscribe()
+    }
+
+    fun unSubscribe() {
+        disposable?.dispose()
+    }
 }
